292343865fb2bb78d91c135a50802b9c43c5fe90
d-kuz <d_kuzyakova@mail.ru>
2023-06-13 22:18:17 +0300

    3-е задание, ошибка 404

---
+++ Task3/src/main/java/MainSevlet.java
@@ -0,0 +1,20 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalTime;
+
+@WebServlet("/time")
+public class MainSevlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.write(String.valueOf(LocalTime.now()));
+        printWriter.close();
+    }
+}
