6e740b8a76b6ad43022121081554389b6c1727e7
d-kuz <d_kuzyakova@mail.ru>
2023-06-18 14:11:32 +0300

    4-е задание

--- Task4/src/main/java/Task4/Main.java
+++ Task4/src/main/java/Task4/Main.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         try {
-            String a = text.readFile("\"C:\\IdeaProjects\\SberPracticle2_2023\\Task4\\src\\CopyBorodino.txt\"");
-            //System.out.println(a);
+            String a = text.readFile("C:\\Users\\Natik\\IdeaProjects\\SberPractice2_2023\\Task4\\src\\main\\java\\Task4\\Borodino.txt");
+            System.out.println(a);
             int c = pars.countWorld(a);
             Writer.Report(c);
         } catch (IOException e) {
--- Task4/src/main/java/Task4/text.java
+++ Task4/src/main/java/Task4/text.java
@@ -7,8 +7,7 @@ public class text {
     public static String  readFile(String path) throws IOException {
         String a="";
         if (path.length() > 0) {
-            try (BufferedReader fileInputStream = new BufferedReader(new FileReader
-                    ("C:\\IdeaProjects\\SberPracticle2_2023\\Lessons5\\src\\Borodino.txt"))) {
+            try (BufferedReader fileInputStream = new BufferedReader(new FileReader(path))) {
                 String b = fileInputStream.readLine();
                 while (b != null) {
                     a += b + " ";
