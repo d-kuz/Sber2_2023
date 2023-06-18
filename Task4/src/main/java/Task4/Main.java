3043fb9f1c2e826c172c8abc085971cbd32fdae8
d-kuz <d_kuzyakova@mail.ru>
2023-06-18 14:17:49 +0300

    4-е задание

--- Task4/src/main/java/Task4/Main.java
+++ Task4/src/main/java/Task4/Main.java
@@ -3,14 +3,10 @@ package Task4;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
+    public static void main(String[] args) throws IOException {
             String a = text.readFile("C:\\Users\\Natik\\IdeaProjects\\SberPractice2_2023\\Task4\\src\\main\\java\\Task4\\Borodino.txt");
             System.out.println(a);
             int c = pars.countWorld(a);
             Writer.Report(c);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
