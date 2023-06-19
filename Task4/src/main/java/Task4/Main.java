9ab0b0bd1e0ae06c3714447a48a9a6264753a836
d-kuz <d_kuzyakova@mail.ru>
2023-06-19 19:58:12 +0300

    4-е задание

--- Task4/src/main/java/Task4/Main.java
+++ Task4/src/main/java/Task4/Main.java
@@ -10,20 +10,25 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @Configuration
 @ComponentScan
 public class Main {
-
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
-        int c = context.getBean(int.class);
-        System.out.println("Количество слов в файле: "+c);
+        HashMap<String,Integer> map =  context.getBean(HashMap.class);
+        for (String key: map.keySet()) {
+            if (!key.equals("0")) {
+                System.out.println(key + " = " + map.get(key)*100/ map.get("0") + "%");
+            }
+        }
 
     }
 
     @Bean("reader")
-    public String readFile() throws IOException {
+    public char[] readFile() throws IOException {
         String path = "C:\\Users\\Natik\\IdeaProjects\\SberPractice2_2023\\Task4\\src\\main\\java\\Task4\\Borodino.txt";
         String a="";
         if (path.length() > 0) {
@@ -35,14 +40,29 @@ public class Main {
                 }
             }
         }
-        return a;
+        char[] chars= a.toCharArray();
+        return chars;
     }
     @Bean("counter")
-    public int count(String a){
-        String[] b = a.split(" ");
-        return b.length;
+    public HashMap<String, Integer> count(char[] a){
+        HashMap<String, Integer> b = new HashMap<>();
+        b.put("0", 0);
+        String world = "";
+        for (char ch: a) {
+            if ((ch >= 'a') & (ch <= 'я') | (ch >= 'А') & (ch <= 'Я') |
+                    (ch == 'ё') | (ch == 'Ё') | (ch == '-')){
+                world +=ch;
+            }else if (world != ""){
+                if (b.get(world) == null){
+                    b.put(world, 1);
+                }else {
+                    b.put(world, b.get(world)+1);
+                }
+                world = "";
+                b.put("0", b.get("0")+1);
+            }
+        }
+        return b;
     }
-
-
 
 }
