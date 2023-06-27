d5b8c358ecc58aa178d28a9bcd8f458bcf98565d
d-kuz <d_kuzyakova@mail.ru>
2023-06-27 16:16:29 +0300

    типы данных

--- MiniNotes/src/main/java/MiniNotes/User.java
+++ MiniNotes/src/main/java/MiniNotes/User.java
@@ -1,4 +1,40 @@
 package MiniNotes;
 
+import java.util.ArrayList;
+
 public class User {
+    private String login;
+    private String password;
+    private String usetId;
+    private ArrayList<Notes> notesList;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsetId() {
+        return usetId;
+    }
+
+
+    public ArrayList<Notes> getNotesList() {
+        return notesList;
+    }
+
+    public void setNotesList(ArrayList<Notes> notesList) {
+        this.notesList = notesList;
+    }
 }
