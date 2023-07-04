558892cd0ae5858d0157cf962ec7d1ea9b8f8315
d-kuz <d_kuzyakova@mail.ru>
2023-07-04 20:58:46 +0300

    задание 8 и 9

--- Task7/src/main/java/com/example/Application.java
+++ Task7/src/main/java/com/example/Application.java
@@ -1,11 +1,12 @@
-package com.example.application;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
-@SpringBootApplication
-@ComponentScan({"com.example.controller"})
+@SpringBootApplication()
+@ComponentScan("com.example.controller")
 public class Application {
     public static void main(String[] args) {
         System.setProperty("server.port", "8081");
