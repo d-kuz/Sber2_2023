1e6a7fb0118e27854a03d86c93e94ff84258c60e
d-kuz <99803153+d-kuz@users.noreply.github.com>
2023-07-07 16:18:47 +0300

    Delete src/main directory

--- src/main/java/com/example/repository/NoteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.repository;
-
-import com.example.model.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface NoteRepository extends JpaRepository<Note, Long> {
-}
